bce042fa0771cb0b18ecd9b8dd8d372ae860e2e4
rayaanoidPrime <rayaan4dev@gmail.com>
2023-04-17 17:20:54 +0000

    dynamodb implementation done

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,8 @@
 INSERT INTO public.users (display_name,email, handle, cognito_user_id)
 VALUES
   ('Rayaan Ghosh', 'rayaan4dev@gmail.com' ,'rayaanoidprime' ,'MOCK'),
-  ('rayaan', 'rayaanghosh18@gmail.com' ,'rayaanoid' ,'MOCK');
+  ('rayaan', 'rayaanghosh18@gmail.com' ,'rayaanoid' ,'MOCK'),
+  ('Londo Mollari', 'londo@centari.com' ,'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
